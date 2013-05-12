75c4be48d10c69b6ba3ce95759a451bf5fcd3bad
Edwin Hoogerbeets <Edwin.Hoogerbeets@palm.com>
2013-05-12 00:51:39 -0700

    The enyo-ilib library is now loadable as a module under nodejs as well as a library under enyo.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 enyo-ilib
 =========
 
-Enyo wrapper for ilib globalization/internationalization library.
+Enyo and nodejs wrapper for ilib globalization/internationalization library.
+
+Both enyo and nodejs wrappers define the $L function used to wrap localizable strings and perform
+translations.
+
+Enyo
+----
+
+To use this in an enyo application, put this library in your enyo libs directory, then edit your package.js:
+
+enyo.depends(
+	"$lib/enyo-ilib",
+	<rest of your dependencies>
+);
+
+Now the "ilib" namespace is available to use in your app.
+
+Nodejs
+------
+
+To use this under nodejs, check out this module under your nodejs app/service. Then, in your js, do:
+
+var ilibmodule = require("./enyo-ilib");
+var ilib = ilibmodule.ilib;
+var $L = ilibmodule.$L;
+
+Now the ilib namespace is available to use as normal.
+
---
+++ nodeglue.js
@@ -0,0 +1,100 @@
+// pass the ilib definition to our callers
+exports.ilib = require("./ilib/js/ilib-dyn.js").ilib;
+
+var path = require("path"),
+	fs = require("fs");
+// var util = require("util");
+
+exports.ilib._load = (function () {
+	var base = path.normalize(path.join(__dirname, "ilib/locale"));
+	
+	if (!fs.existsSync(path.join(base, "localeinfo.json"))) {
+		base = "/usr/palm/ilib/locale";
+	}
+	
+	function nextFile(context, root, paths, results, callback, json) {
+		//util.print("node loader:  " + (json ? "succeeded" : "failed") + "\n");
+		results.push(json ? JSON.parse(json) : undefined);
+		if (paths.length > 0) {
+			loadFiles(context, root, paths, results, callback);
+		} else {
+			// only call the callback at the end of the chain of files
+			if (typeof(callback) === 'function') {
+				callback(results);
+			}
+		}
+	}
+	
+	function loadFiles(context, root, paths, results, callback) {
+		if (paths.length > 0) {
+			var filename = paths.shift();
+			var filepath = path.join(root, filename);
+			//util.print("node loader: attempting sync load " + filepath + "\n");
+			fs.readFile(filepath, "utf-8", function(err, json) {
+				if (err) {
+					filepath = path.join("resources", filename);
+					//util.print("node loader: attempting sync load " + filepath + "\n");
+					fs.readFile(filepath, "utf-8", function(err, json) {
+						nextFile(context, root, paths, results, callback, err ? undefined : json);
+					});
+				} else {
+					nextFile(context, root, paths, results, callback, json);
+				}
+			});
+		}
+	}
+	return function(paths, sync, params, callback) {
+		var root = (params && params.base) ? path.normalize(params.base) : base;
+
+		//util.print("node loader: attempting to load paths " + JSON.stringify(paths) + "\n");
+		if (sync) {
+			var ret = [];
+			
+			// synchronous
+			paths.forEach(function (p) {
+				var filepath = path.join(root, p);
+				//util.print("node loader: attempting sync load " + filepath + "\n");
+				var json;
+				if (fs.existsSync(filepath)) {
+					json = fs.readFileSync(filepath, "utf-8");
+				} else {
+					filepath = path.join(root, "resources", p);
+					//util.print("node loader: attempting sync load " + filepath + "\n");
+					if (fs.existsSync(filepath)) {
+						json = fs.readFileSync(filepath, "utf-8");
+					}
+				}
+				//util.print("node loader:  " + (json ? "succeeded" : "failed") + "\n");
+				ret.push(json ? JSON.parse(json) : undefined);
+			});
+
+			// only call the callback at the end of the chain of files
+			if (typeof(callback) === 'function') {
+				callback(ret);
+			}
+
+			return ret;
+		}
+
+		// asynchronous
+		var results = [];
+		loadFiles(this, root, paths, results, callback);
+	};
+	return undefined;
+})();
+
+exports.$L = (function() {
+	var lfunc = function (string) {
+		var str = $L.rb.getString(string); 
+		return str.toString();
+	};
+	lfunc.rb = new exports.ilib.ResBundle({
+		type: "html",
+		name: "strings",
+		lengthen: true		// if pseudo-localizing, this tells it to lengthen strings
+	});
+	//util.print("node loader: the resource bundle is now " + JSON.stringify(lfunc.rb) + "\n");
+	return lfunc;
+})();
+
+
