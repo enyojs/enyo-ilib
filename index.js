ef04f07ba9d57689d55d499db7679d82c644fac8
Ryan Duffy <ryan.duffy@lge.com>
2015-06-30 18:00:22 -0500

    patch ilib circular dependencies
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var
 // ensure the locale-specific date types are included
 require('./lib/dates');
 
+// patch the circular dependencies between ilib date and calendar objects
+require('./lib/patch');
+
 ilib.enyo = glue;
 ilib.enyo.version = '2.6.0-pre.5-dev';
 
