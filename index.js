183132da8da7296cc486a987e98a72fb6c369b70
Ryan Duffy <ryan.duffy@lge.com>
2015-08-27 09:39:52 -0500

    Revert "patch ilib circular dependencies"
    This reverts commit 328bce3d1c76348f93cc45ebefc63a5a71095bb7.

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var
 // ensure the locale-specific date types are included
 require('./src/dates');
 
-// patch the circular dependencies between ilib date and calendar objects
-require('./src/patch');
-
 ilib.enyo = glue;
 ilib.enyo.version = '2.6.0-pre.14.1';
 
